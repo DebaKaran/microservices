00ac07dfc52659da883d008df9449fafe6daa4f8
DebaKaran <karan.deba@gmail.com>
2025-07-02 00:20:43 +0530

    test: add integration test for CustomerRepository using @DataJpaTest

---
+++ section-02/accounts/src/test/java/com/easybytes/accounts/repository/CustomerRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.easybytes.accounts.repository;
+
+import com.easybytes.accounts.entity.Customer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class CustomerRepositoryTest {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Test
+    public void testFindByMobileNumber_shouldReturnCustomer() {
+        // Given
+        Customer customer = new Customer();
+        customer.setName("Test User");
+        customer.setEmail("test@example.com");
+        customer.setMobileNumber("9999999999");
+
+        // Set required base fields
+        customer.setCreatedAt(LocalDateTime.now());
+        customer.setCreatedBy("test-run");
+
+        customerRepository.save(customer);
+
+        //when
+        Optional<Customer> foundCustomer  =
+                customerRepository.findByMobileNumber("9999999999");
+
+        //then
+        assertThat(foundCustomer).isPresent();
+        assertThat(foundCustomer.get().getEmail()).isEqualTo("test@example.com");
+
+    }
+}
